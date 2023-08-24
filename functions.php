76222884b02e9cdaaa4429e0efa32502accf17e4
Ziddykins <Jeff0012@gmail.com>
2023-08-24 11:53:16 -0400

    k

--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php
+    include('db.php');
     function get_mysql_datetime() {
         return date("Y-m-d H:i:s", strtotime("now"));
     }
-?>
+
+    function get_user($email, $type) {
+        global $db;
+        $table = $type == 'account' ? 'SQL_ACCT_TBL' : 'SQL_CHAR_TBL';
+
+        $sql_query = 'SELECT * FROM ' . $_ENV[$table] . ' WHERE email = ?';
+    
+        $prepped   = $db->prepare($sql_query);
+        $prepped->bind_param('s', $email);
+        $prepped->execute();
+
+        $result = $prepped->get_result();
+        $user = $result->fetch_assoc();
+
+        return $user;
+    }
