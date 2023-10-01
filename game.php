a03127ab5b6443e5fec5448d0d743fcd47042612
Ziddykins <Jeff0012@gmail.com>
2023-10-01 18:57:37 +0000

    k

--- game.php
+++ game.php
@@ -228,7 +228,6 @@
                                 </ul>
                             </li>
                         </ul>
-=======
                         <!-- broke? mb-sm-auto  -->
                         <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                             <a href="#sub-character-menu" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
