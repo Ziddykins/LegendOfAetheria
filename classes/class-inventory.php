b90972c5d8f7862b67f67d19f8bc0d583a36b55f
ZippyZipZop <145229762+ZippyZipZop@users.noreply.github.com>
2023-09-30 22:40:27 -0400

    update classes/class-inventory.php

--- classes/class-inventory.php
+++ classes/class-inventory.php
@@ -1,8 +1,15 @@
 <?php
     class Inventory {
         protected $accountID;
+        protected current_weight;
+        protected maximum_weight;
+        protected slots;
         
         public function __construct($playerID) {
-        $this->accountID = $accountID;
+            $this->accountID = $accountID;
+            $this->current_weight = 0;
+            $this->maximum_weight = 100;
+            $this->slots = 25;
+        }
     }
 ?>
