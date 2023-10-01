8fde89b0b186e7566c5aa966e22548b7f0c3923a
ZippyZipZop <145229762+ZippyZipZop@users.noreply.github.com>
2023-09-30 22:16:15 -0400

    update classes/class-familiar.php

---
+++ classes/class-familiar.php
@@ -0,0 +1,18 @@
+<?php
+    class Familiar {
+        protected $accountID;
+        protected $name;
+        
+        public function __construct($playerID) {
+            $this->accountID = $accountID;
+        }
+        
+        protected function set_name($name) {
+            $this->name = $name;
+        }
+        
+        protected function get_name() {
+            return $this->name;
+        }
+    }
+?>
