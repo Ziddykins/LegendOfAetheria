d4305f2d821c77d97459f4187590f8e59f95b2ca
ZippyZipZop <145229762+ZippyZipZop@users.noreply.github.com>
2023-09-30 22:16:45 -0400

    update classes/class-player.php

--- classes/class-player.php
+++ classes/class-player.php
@@ -6,29 +6,4 @@
             $this->accountID = $accountID;
         }
     }
-    
-    class Inventory {
-        protected $accountID;
-        
-        public function __construct($playerID) {
-            $this->accountID = $accountID;
-        }
-    }
-    
-    class Familiar {
-        protected $accountID;
-        protected $name;
-        
-        public function __construct($playerID) {
-            $this->accountID = $accountID;
-        }
-        
-        protected function set_name($name) {
-            $this->name = $name;
-        }
-        
-        protected function get_name() {
-            return $this->name;
-        }
-    }
-}
+?>
