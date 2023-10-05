c9aea3f81497343c97aff069e8e773b0c62d846f
Jeff <Jeff0012@hotmail.com>
2023-10-05 14:26:12 -0400

    update pages/game-profile.php

--- pages/game-profile.php
+++ pages/game-profile.php
@@ -4,23 +4,6 @@
 
 ?>
 
-<<<<<<< HEAD
-<div class="container ">
-    <div class="row pt-5">
-        <div class="col">
-            <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">
-                    Account
-                </a>
-                <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">
-                    Character
-                </a>
-                <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">
-                    Messages
-                </a>
-                <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">
-                    SPOTIFY PREEMIIUUUUM
-=======
 <div class="container text-white">
     <div class="row pt-5">
         <div class="col">
@@ -28,15 +11,14 @@
                 <a class="list-group-item list-group-item-action active bg-dark text-white" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">
                     Account
                 </a>
-                <a class="list-group-item list-group-item-action bg-dark text-white" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">
+                <a class="list-group-item list-group-item-action bg-dark text-white" id="lislt-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">
                     Character
                 </a>
                 <a class="list-group-item list-group-item-action bg-dark text-white" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">
                     Messages
                 </a>
                 <a class="list-group-item list-group-item-action bg-dark text-white" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">
-                    SPOTIFY PREMIUMMMMMM
->>>>>>> 9806c21609a4f9958274f1980a2e43cead173763
+                    hi
                 </a>
             </div>
         </div>
