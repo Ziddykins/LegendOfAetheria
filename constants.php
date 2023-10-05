75ff8082bc666139faa950ff00941a47c46f734e
Jeff <Jeff0012@hotmail.com>
2023-10-05 14:31:45 -0400

    update constants.php

--- constants.php
+++ constants.php
@@ -1,10 +1,7 @@
 <?php
 
     define('MAX_ASSIGNABLE_AP', 40);
-<<<<<<< HEAD
     define('ENERGY_PER_TICK', 3);
-=======
->>>>>>> 9806c21609a4f9958274f1980a2e43cead173763
     
     enum Races {
         public static function random(): self {
@@ -48,27 +45,13 @@
         case ROOTED = 1024;
     };
 
-<<<<<<< HEAD
+
     enum Weather {
-=======
-    enum Weather: int {
->>>>>>> 9806c21609a4f9958274f1980a2e43cead173763
         public static function random(): self {
             $count = count(self::cases()) - 1;
             return self::cases()[rand(0, $count)];
         }
-<<<<<<< HEAD
-   
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-   
->>>>>>> ef4665f (wewp)
-=======
-   
->>>>>>> ef4665f (wewp)
->>>>>>> 9806c21609a4f9958274f1980a2e43cead173763
+
         public static function name_to_value(string $name): string {
             foreach (self::cases() as $weather) {
                 if ($name === $weather->name){
@@ -77,7 +60,6 @@
             }
             throw new \ValueError("$name is not a valid backing value for enum " . self::class);
         }
-<<<<<<< HEAD
   
         public function icon(): string {
             return match($this) {
@@ -102,33 +84,4 @@
         case REQUEST;
         case NONE;
     };
-    
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> ef4665f (wewp)
-=======
-  
->>>>>>> ef4665f (wewp)
-        public function icon(): string {
-            return match($this) {
-                Weather::CLOUDY =>  '<i class="bi bi-cloud-fill"></i>',
-                Weather::SUNNY  =>  '<i class="bi bi-brightness-high-fill"></i>',
-                Weather::HAILING => '<i class="bi bi-cloud-hail-fill"></i>',
-                Weather::SNOWING => '<i class="bi bi-cloud-snow-fill"></i>',
-                Weather::RAINING => '<i class="bi bi-cloud-lightning-rain-fill"></i>'
-            };
-        }
-
-        case SUNNY = 1;
-        case RAINING = 2;
-        case HAILING = 4;
-        case CLOUDY = 8;
-        case SNOWING = 16;
-    }
->>>>>>> 9806c21609a4f9958274f1980a2e43cead173763
 ?>
-
-
