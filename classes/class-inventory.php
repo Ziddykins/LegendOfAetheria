66f13f67663b3c37926291371c95a4ba66c13623
Jeff <Jeff0012@hotmail.com>
2023-10-20 00:31:37 -0400

    update classes/class-inventory.php

--- classes/class-inventory.php
+++ classes/class-inventory.php
@@ -1,15 +1,15 @@
 <?php
     class Inventory {
         protected $accountID;
-        protected $current_weight;
-        protected $maximum_weight;
-        protected $slots;
+        protected $currentWeight;
+        protected $maximumWeight;
+        protected $totalSlots;
         
         public function __construct($playerID) {
             $this->accountID = $accountID;
-            $this->current_weight = 0;
-            $this->maximum_weight = 100;
-            $this->slots = 25;
+            $this->currentWeight = 0;
+            $this->maximumWeight = 100;
+            $this->totalSlots = 25;
         }
     }
 ?>
