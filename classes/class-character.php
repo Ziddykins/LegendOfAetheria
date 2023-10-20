112d901f3a5728dfa1dbc0e7875866f74523b74d
Jeff <Jeff0012@hotmail.com>
2023-10-20 00:29:19 -0400

    update classes/class-character.php

--- classes/class-character.php
+++ classes/class-character.php
@@ -1,18 +1,18 @@
 <?php  
     class Character {
         protected $accountID;
-        protected $name;
+        protected $charName;
         
         public function __construct($playerID) {
             $this->accountID = $accountID;
         }
         
         protected function set_name($name) {
-            $this->name = $name;
+            $this->charName = $name;
         }
         
         protected function get_name() {
-            return $this->name;
+            return $this->charName;
         }
     }
 ?>
