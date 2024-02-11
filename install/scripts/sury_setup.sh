b605b0f40fd791174b52404bc58d1795372427d2
root <root@localhost.localdomain>
2024-02-11 05:59:59 -0500

    still updating autoconfig script aahhhhloarrrd

--- install/scripts/sury_setup.sh
+++ install/scripts/sury_setup.sh
@@ -8,5 +8,5 @@ fi
 ${SUDO} apt-get update
 ${SUDO} apt-get -y install lsb-release ca-certificates curl
 ${SUDO} curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
-${SUDO} sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+${SUDO} sh -c 'echo "deb [trusted=yes signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 ${SUDO} apt-get update
