12029f78e56299e1a139002644ff4f4c8d3541cb
Ziddykins <ziddy@memelife.ca>
2025-03-03 18:16:37 -0500

    Update AutoInstaller and scripts: fix virtual host configuration, streamline service start commands, and enhance PHP repository setup.

--- install/scripts/sury_setup_ubnt.sh
+++ install/scripts/sury_setup_ubnt.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:ondrej/php
+apt install -y software-properties-common
+yes | LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
+yes | LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
 apt update
